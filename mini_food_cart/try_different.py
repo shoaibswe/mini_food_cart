7e6e7a77dd91353985300c00eb6e95d2729c49ac
shoaibswe <shoaibswe@gmail.com>
2025-09-15 00:08:44 +0600

    feat: new way of using methods

---
+++ mini_food_cart/try_different.py
@@ -0,0 +1,71 @@
+#rastourant menu
+from collections import Counter
+
+menue ={
+    "burger": 5.99,
+    "fries": 2.99,
+    "soda": 1.49,
+    "salad": 4.49,
+    "ice cream": 3.49
+}
+
+def display_menu():
+    print("---- Menu: ----")
+    for item, price in menue.items():
+        print(f"{item},${price}")
+    print("---------------")
+
+def take_order():
+    order_list =[]
+    print("Enter item name or number or q to exit")
+    while True:
+        item = input().lower()
+        if item == 'q':
+            break
+        elif menue.get(item) is None:
+            print("! Item not found. Please try again.")
+        elif item in menue:
+            order_list.append(item)
+            print(f"{item.title()} : {menue.get(item,0)} added to your order.")
+            # print(f"Total : {sum(menue.get(item,0) for item in order_list)}")
+            print("Add more item or q to exit")
+        else:
+            print("! Something went wrong. Please try again.")
+    return order_list
+
+def subtotal(order_list):
+    item_count = Counter(order_list)
+    print("==== Your Order: ====")
+    # print(item_count):  Counter({'salad': 2, 'fries': 3, 'burger': 1})
+    for item, quantity in item_count.items():
+        price = menue.get(item,0)
+        subtotal = price * quantity
+        print(f"- {item.title()}, {quantity}pc : ${subtotal:.2f}")
+    return item_count
+
+def total(item_count):
+    total = 0.0
+    # for item, quantity in item_count.items():
+    #     price = menue.get(item,0)
+    #     subtotal = price * quantity
+    #     total += subtotal
+    total = sum(menue.get(item,0) * quantity for item, quantity in item_count.items())
+    return total
+
+
+def main():
+        
+        display_menu()
+
+        total_amount = total(
+            subtotal(
+                take_order()))
+        print("--------------------")
+        print(f"Total amount: ${total_amount:.2f}")
+        print("====================")
+        print("Thank you for your order!")
+
+
+if __name__ == "__main__":
+    main()
+
